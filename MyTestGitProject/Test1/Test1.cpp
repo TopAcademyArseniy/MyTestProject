9eae0f37e95ef69dc5c1dd0f5d1e48172bec6afb
Arseniy <topacademyarseniy@gmail.com>
2022-05-20 20:19:52 +0500

    Initial commit

---
+++ MyTestGitProject/Test1/Test1.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++) {
+		
+			std::cout << '*';
+		}
+		std::cout << std::endl;
+	}
+
+    std::cout << "Hello World!\n";
+}
